a803b291bd32d943b0e6c93cc42420a964382df2
Abbi Engel <abbi.engel33@gmail.com>
2022-02-16 13:51:43 -0600

    password loop

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-import input from 'input';
+import input from "input";
 
 class App {
-    constructor(password) {
-        this.answer = 'password123';
-    }
+  constructor(password) {
+    this.answer = "password123";
+  }
 
-    async main() {
-        const resp = await input.text('What is the password?');
-        if (resp == this.answer) {
-            console.log('Correct!')
-        } else {
-            console.log('Incorrect.');
-        }
-
+  async main() {
+    let resp;
+    while (resp != this.answer) {
+      resp = await input.text("What is the password?");
     }
+    if ((resp = this.answer)) {
+      console.log("Correct!");
+    } else {
+      console.log("Incorrect.");
+    }
+  }
 }
 
-const app = new App('password123');
+const app = new App("password123");
 app.main();
