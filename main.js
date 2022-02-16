8e67ce076fc71b42dda4ddd26762d43a6d5fe336
Abbi Engel <abbi.engel33@gmail.com>
2022-02-16 13:29:40 -0600

    password

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-class Pet {
-    constructor(breed, name, gender, species) {
-        this.breed = breed;
-        this.name = name;
-        this.gender = gender;
-        this.species = species; 
+import input from 'input';
+
+class App {
+    constructor(password) {
+        this.answer = 'password123';
     }
 
+    async main() {
+        const resp = await input.text('What is the password?');
+        if (resp == this.answer) {
+            console.log('Correct!')
+        } else {
+            console.log('Incorrect.');
+        }
 
-describe() {
-    if (this.gender == 'male') {
-    console.log( `My ${this.species}s name is ${this.name} and he is a ${this.gender} ${this.breed}.`
-    )} else {
-        console.log (`My ${this.species} is named ${this.name} and is a ${this.breed}`)
     }
-   }
 }
 
-function main() {
-    const myPet = new Pet('saint bernard', 'Otis', 'male', 'dog');
-    myPet.describe();
-}
-
-main();
+const app = new App('password123');
+app.main();
