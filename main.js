28c94fead040c6672388e6bb72daba27b291e3e3
Abbi Engel <abbi.engel33@gmail.com>
2022-02-16 10:09:57 -0600

    decribe method

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 class Pet {
-    constructor(species, name) {
-        this.species = species;
+    constructor(breed, name, gender, species) {
+        this.breed = breed;
         this.name = name;
+        this.gender = gender;
+        this.species = species; 
     }
+
+
+describe() {
+    console.log( `My ${this.species}s name is ${this.name} and they are a ${this.breed}. He is a good ${this.gender}.`
+    );
+   }
 }
 
 function main() {
-    const myPet = new Pet('dog', 'Otis');
-    console.log(myPet);
+    const myPet = new Pet('saint bernard', 'Otis', 'boy', 'dog');
+    myPet.describe();
 }
 
 main();
